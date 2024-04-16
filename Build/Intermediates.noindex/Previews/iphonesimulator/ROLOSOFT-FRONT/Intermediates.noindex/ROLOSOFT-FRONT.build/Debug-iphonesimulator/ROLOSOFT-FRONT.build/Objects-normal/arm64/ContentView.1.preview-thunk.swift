48b6a439dbcc1e026806657b97ec9d5a41296977
Beltry201 <beltrangonzalezdavid@gmail.com>
2024-04-15 20:48:04 -0600

    Initial Commit

---
+++ Build/Intermediates.noindex/Previews/iphonesimulator/ROLOSOFT-FRONT/Intermediates.noindex/ROLOSOFT-FRONT.build/Debug-iphonesimulator/ROLOSOFT-FRONT.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -0,0 +1,32 @@
+@_private(sourceFile: "ContentView.swift") import ROLOSOFT_FRONT
+import func SwiftUI.__designTimeBoolean
+import func SwiftUI.__designTimeInteger
+import protocol SwiftUI.PreviewProvider
+import func SwiftUI.__designTimeString
+import func SwiftUI.__designTimeFloat
+import struct SwiftUI.EmptyView
+import protocol SwiftUI.View
+import SwiftUI
+
+extension ContentView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/David/Documents/Tec/Semestre 5/Ciberseguridad - swift/reto/ROLOSOFT-FRONT/ROLOSOFT-FRONT/ContentView.swift", line: 12)
+        VStack {
+            Image(systemName: __designTimeString("#557.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: "globe"))
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(__designTimeString("#557.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "Hello, world!"))
+        }
+        .padding()
+    
+#sourceLocation()
+    }
+}
+
+import struct ROLOSOFT_FRONT.ContentView
+#Preview {
+    ContentView()
+}
+
+
+
---
+++ ROLOSOFT-FRONT/ROLOSOFT_FRONTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROLOSOFT_FRONTApp.swift
+//  ROLOSOFT-FRONT
+//
+//  Created by David Beltran on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ROLOSOFT_FRONTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
