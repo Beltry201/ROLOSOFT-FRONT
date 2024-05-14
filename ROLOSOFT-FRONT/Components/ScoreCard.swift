551c111f3c6eb2ddcffd49da70a0ae9814bf451c
Juan_Bedoya_Barbosa <102477075+JBedoya-Barbosa@users.noreply.github.com>
2024-05-14 17:52:30 -0600

    ScoreCard improved

--- ROLOSOFT-FRONT/Components/ScoreCard.swift
+++ ROLOSOFT-FRONT/Components/ScoreCard.swift
@@ -11,21 +11,31 @@ struct ScoreCard: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
-                .fill(LinearGradient(gradient: Gradient(colors: [.red, .orange]), startPoint: .bottom, endPoint: .top))
-                .frame(height: 150)
-                .shadow(radius: 10)
+                .fill(LinearGradient(
+                        gradient: Gradient(colors: [Color(red: 0.854, green: 0.000, blue: 0.176, opacity: 0.85),
+                                                    Color(red: 0.882, green: 0.341, blue: 0.008, opacity: 0.85)]),
+                        startPoint: .bottom,
+                        endPoint: .top))
+                    .frame(height: 180)
+                    .shadow(color: Color.black.opacity(0.10), radius: 24, x: 0, y: 0)
 
             HStack {
                 TeamScoreView(teamName: "Independiente Santa Fé", teamLogo: "santafe_logo")
                 
-                Text("2 - 1")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
+                VStack {
+                    Spacer()
+                    Text("2 - 1")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Spacer()
+                }
                 
                 TeamScoreView(teamName: "América de Cali", teamLogo: "americadecali_logo")
             }
+            .padding()
         }
+        .frame(height: 180)
     }
 }
 
@@ -42,9 +52,16 @@ struct TeamScoreView: View {
                 .multilineTextAlignment(.center)
                 .foregroundColor(.white)
                 .fixedSize(horizontal: false, vertical: true)
+                .padding(.bottom, 2)
+            Text("(12' Jugador)")
+                .font(.caption2)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .fontWeight(.light)
+                .fixedSize(horizontal: false, vertical: true)
+            Spacer()
         }
         .frame(maxWidth: 120)
-        .padding(.vertical, 4)
     }
 }
 
