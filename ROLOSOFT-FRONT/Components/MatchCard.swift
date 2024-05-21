8dff806c240730f84e0c769bff6e9b5c00d7d7c9
Juan_Bedoya_Barbosa <102477075+JBedoya-Barbosa@users.noreply.github.com>
2024-05-20 20:18:09 -0600

    HomeView upgraded

---
+++ ROLOSOFT-FRONT/Components/MatchCard.swift
@@ -0,0 +1,90 @@
+//
+//  ScoreCard.swift
+//  ROLOSOFT-FRONT
+//
+//  Created by Juan Camilo Bedoya Barbosa on 13/05/24.
+//
+
+import SwiftUI
+
+struct MatchCard: View {
+    var teamA: ScoreCardTeamData
+    var teamB: ScoreCardTeamData
+    var dateString: String
+    var isMyMatch: Bool? = false
+    
+    var body: some View {
+        VStack {
+            Text(dateString)
+                .foregroundColor(isMyMatch! ? .white : .black)
+                .font(.caption)
+            
+            HStack(alignment: .top) {
+                TeamBox(name: teamA.name, logoUrl: teamA.logo, isMyMatch: isMyMatch!)
+                
+                Spacer()
+                
+                VStack {
+                    Text("VS")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(isMyMatch! ? .white : .black)
+                }
+                
+                Spacer()
+                
+                TeamBox(name: teamB.name, logoUrl: teamB.logo, isMyMatch: isMyMatch)
+            }
+        }
+        .padding()
+        .modifier(BackgroundModifier(isMyMatch: isMyMatch!))
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+    }
+}
+
+struct BackgroundModifier: ViewModifier {
+    var isMyMatch: Bool? = false
+
+    func body(content: Content) -> some View {
+        if let isMyMatch = isMyMatch, isMyMatch {
+            return AnyView(content
+                .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.854, green: 0.000, blue: 0.176, opacity: 0.85), Color(red: 0.882, green: 0.341, blue: 0.008, opacity: 0.85)]), startPoint: .bottom, endPoint: .top))
+            )
+        } else {
+            return AnyView(content
+                .background(Color.white)
+            )
+        }
+    }
+}
+
+struct TeamBox: View {
+    var name: String
+    var logoUrl: String
+    var isMyMatch: Bool? = false
+
+    var body: some View {
+        VStack(alignment: .center) {
+            URLImage(url: logoUrl)
+                .frame(width: 50, height: 50)
+            Text(name)
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+                .foregroundColor(isMyMatch! ? .white : .black)
+                .fixedSize(horizontal: false, vertical: true)
+        }
+        .frame(maxWidth: 120)
+    }
+}
+
+struct ScoreCardTeamData {
+    var name: String
+    var logo: String
+}
+
+struct ScoreCardPreview: PreviewProvider {
+    static var previews: some View {
+        MatchCard(teamA: ScoreCardTeamData(name: "EquipoA ak adkadla da", logo: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"), teamB: ScoreCardTeamData(name: "EquipoB", logo: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"), dateString: "4:00 PM")
+    }
+}
--- ROLOSOFT-FRONT/Views/Home/HomeView.swift
+++ ROLOSOFT-FRONT/Views/Home/HomeView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+// Create dummy team data
+let teamA = ScoreCardTeamData(
+    name: "IDS",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/5/58/Escudo_de_Independiente_Santa_Fe.png"
+)
+
+let teamB = ScoreCardTeamData(
+    name: "AN",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Atlético_Nacional.png"
+)
+
 struct HomeView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @ObservedObject var authService: AuthService // Add authService as an observed object
@@ -26,36 +37,22 @@ struct HeaderView: View {
     var body: some View {
         VStack(alignment: .center) {
             HStack {
-                // Logo view
+                
                 Image("frisa-logox4")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(height: 30)
                 Spacer()
-                // Notifications icon view
+                
                 Image(systemName: "bell")
                     .font(.title)
-                    .foregroundColor(.blue) // Customize color as needed
+                    .foregroundColor(.blue)
             }
             .padding(.horizontal)
     
             CalendarComponent().padding()
             
-            // Create dummy team data
-            let teamA = ScoreCardTeamData(
-                name: "IDS",
-                logo: "https://upload.wikimedia.org/wikipedia/commons/5/58/Escudo_de_Independiente_Santa_Fe.png",
-                score: 2
-            )
-            
-            let teamB = ScoreCardTeamData(
-                name: "AN",
-                logo: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Atlético_Nacional.png",
-                score: 1
-            )
-            
-            // Use the ScoreCard with dummy data
-            ScoreCard(teamA: teamA, teamB: teamB, hour: "4:00 PM")
+            MatchCard(teamA: teamA, teamB: teamB, dateString: "4:00 PM", isMyMatch: true)
                 .padding(.horizontal)
         }
         .padding(.bottom)
@@ -65,24 +62,29 @@ struct HeaderView: View {
 struct BodyView: View {
     var body: some View {
         List {
-            ForEach(newsData, id: \.title) { news in
-                NewsCard(
-                    title: news.title,
-                    date: news.date,
-                    desc: news.desc,
-                    imgUrl: news.imgUrl
+            ForEach(matchCardsData, id: \.id) { match in
+                MatchCard(
+                    teamA: match.teamA,
+                    teamB: match.teamB,
+                    dateString: match.dateString
                 )
             }
+            .listRowBackground(Color.gray.opacity(0))
+            .listRowInsets(.init(
+                top: 0,
+                leading: 0,
+                bottom: 8,
+                trailing: 0)
+            )
         }
     }
 }
 
-let newsData = [
-    (title: "Title 1", date: "Date 1", desc: "Description 1", imgUrl: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"),
-    (title: "Title 2", date: "Date 2", desc: "Description 2", imgUrl: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"),
-    (title: "Title 3", date: "Date 3", desc: "Description 3", imgUrl: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"),
-    (title: "Title 4", date: "Date 4", desc: "Description 4", imgUrl: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png"),
-    (title: "Title 5", date: "Date 5", desc: "Description 5", imgUrl: "https://www.pikpng.com/pngl/m/430-4309067_escudo-del-club-independiente-santa-fe-cardenales-primer.png")
+let matchCardsData = [
+    (id: 1, teamA: teamA, teamB: teamB, dateString: "4:00 PM"),
+    (id: 2, teamA: teamA, teamB: teamB, dateString: "4:00 PM"),
+    (id: 3, teamA: teamA, teamB: teamB, dateString: "4:00 PM"),
+    (id: 4, teamA: teamA, teamB: teamB, dateString: "4:00 PM")
 ]
 
 struct NavigationBarItemView: View {
